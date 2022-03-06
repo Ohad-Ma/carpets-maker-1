eadb2bf959c109d1e68a4d48a0a35070ae859141
Ohad Maday <73055568+Ohad-Ma@users.noreply.github.com>
2022-03-06 19:14:57 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -13,13 +13,16 @@
 using namespace std;
 using namespace ariel;
 
-float floatGenerator(bool type);
-char randomSymbol();
-int oddIntegerGenerator(bool type);
-int evenIntegerGenerator(bool type);
+float floatGenerator(bool type); // generates float number (negative/positive)
+char randomSymbol(); // generates random special character
+int oddIntegerGenerator(bool type); // generates random odd number (negative/positive)
+int evenIntegerGenerator(bool type); // generates random even number (negative/positive)
 
 char symbA = randomSymbol(), symbB = randomSymbol();
 
+int even_number1 = evenIntegerGenerator(true), even_number2 = evenIntegerGenerator(true), nega_even_number1 = evenIntegerGenerator(false), nega_even_number2 = evenIntegerGenerator(false); 
+int odd_number1 = oddIntegerGenerator(true), odd_number2 = oddIntegerGenerator(true), nega_odd_number1 = oddIntegerGenerator(false), nega_odd_number2 = oddIntegerGenerator(false);
+float fraction = floatGenerator(true), nega_fraction = floatGenerator(false), fraction2 = floatGenerator(true), nega_fraction2 = floatGenerator(false) ;
 //printf( "got here! 22");
 /** 
  * Returns the input string without the whitespace characters: space, newline and tab.
@@ -39,20 +42,18 @@ string no_spaces(string input) {
  * 
  */
 TEST_CASE("Even ROWS * COLS"){
-    int a = evenIntegerGenerator(true), b = evenIntegerGenerator(true), c = evenIntegerGenerator(false), d = evenIntegerGenerator(false); 
-    int e = oddIntegerGenerator(true), g = oddIntegerGenerator(false);
-     CHECK_THROWS(mat(2,2,'$','!')); // even * even (positive)
-     CHECK_THROWS(mat(b,a,symbA,symbB)); // even * even (positive)
-     CHECK_THROWS(mat(a,a,symbA,symbB)); // even * even (same positive)
-     CHECK_THROWS(mat(a,c,symbA,symbB)); // even * even (positive - negative)
-     CHECK_THROWS(mat(c,a,symbA,symbB)); // even * even (negative - positive)
-     CHECK_THROWS(mat(c,d,symbA,symbB)); // even * even (negative - negative)
-     CHECK_THROWS(mat(c,c,symbA,symbB)); // even * even (same negative)
-     CHECK_THROWS(mat(a,e,symbA,symbB)); // even * odd (positive - positive)
-     CHECK_THROWS(mat(e,a,symbA,symbB)); // odd * even (positive - positive)
-     CHECK_THROWS(mat(a,g,symbA,symbB)); // even * odd (positive - negative)
-     CHECK_THROWS(mat(g,a,symbA,symbB)); // odd * even (negative - positive)
-     CHECK_THROWS(mat(c,g,symbA,symbB)); // even * odd (negative - negative)
+     CHECK_THROWS(mat(even_number1,even_number2,symbA,symbB)); // even * even (positive)
+     CHECK_THROWS(mat(even_number2,even_number1,symbA,symbB)); // even * even (positive)
+     CHECK_THROWS(mat(even_number1,even_number1,symbA,symbB)); // even * even (same positive)
+     CHECK_THROWS(mat(even_number1,nega_even_number1,symbA,symbB)); // even * even (positive - negative)
+     CHECK_THROWS(mat(nega_even_number1,even_number1,symbA,symbB)); // even * even (negative - positive)
+     CHECK_THROWS(mat(nega_even_number1,nega_even_number2,symbA,symbB)); // even * even (negative - negative)
+     CHECK_THROWS(mat(nega_even_number1,nega_even_number1,symbA,symbB)); // even * even (same negative)
+     CHECK_THROWS(mat(even_number1,odd_number1,symbA,symbB)); // even * odd (positive - positive)
+     CHECK_THROWS(mat(odd_number1,even_number1,symbA,symbB)); // odd * even (positive - positive)
+     CHECK_THROWS(mat(even_number1,nega_odd_number1,symbA,symbB)); // even * odd (positive - negative)
+     CHECK_THROWS(mat(nega_odd_number1,even_number1,symbA,symbB)); // odd * even (negative - positive)
+     CHECK_THROWS(mat(nega_even_number1,nega_odd_number1,symbA,symbB)); // even * odd (negative - negative)
 }
 
 
@@ -61,10 +62,10 @@ TEST_CASE("Even ROWS * COLS"){
  * 
  */
 TEST_CASE("Negative ROWS & COLS") {
-    int a = oddIntegerGenerator(false), b = oddIntegerGenerator(true), c = oddIntegerGenerator(false);
-    CHECK_THROWS(mat(a,b,symbA,symbB));
-    CHECK_THROWS(mat(b,c,symbA,symbB));
-    CHECK_THROWS(mat(a,c,symbA,symbB));
+    CHECK_THROWS(mat(nega_odd_number1,odd_number1,symbA,symbB)); // odd * odd (negative - positive)
+    CHECK_THROWS(mat(odd_number1,nega_odd_number2,symbA,symbB)); // odd * odd (positive - negative)
+    CHECK_THROWS(mat(nega_odd_number1,nega_odd_number2,symbA,symbB)); // odd * odd (negative - negative)
+    CHECK_THROWS(mat(nega_odd_number1,nega_odd_number1,symbA,symbB)); // odd * odd (negative - negative)
 
 }
 
@@ -73,22 +74,20 @@ TEST_CASE("Negative ROWS & COLS") {
  * 
  */
 TEST_CASE("Special characters arguments"){
-    int a = oddIntegerGenerator(false), b = oddIntegerGenerator(true), c = oddIntegerGenerator(false);
-    float z = floatGenerator(true), x = floatGenerator(false);
-    CHECK_THROWS(mat(b,b,a,b));
-    CHECK_THROWS(mat(b,b,b,b));
+    CHECK_THROWS(mat(odd_number1,odd_number1,nega_odd_number1,odd_number1));
+    CHECK_THROWS(mat(odd_number1,odd_number1,odd_number1,odd_number1));
     CHECK_THROWS(mat(symbA,symbB,symbA,symbB));
     CHECK_THROWS(mat(symbA,symbA,symbA,symbA));
-    CHECK_THROWS(mat(symbA,b,b,symbB));
-    CHECK_THROWS(mat(b,symbB,b,symbB));
-    CHECK_NOTHROW(mat(b,b,symbA,b)); // should print symbA filled matrix size of b*b
-    CHECK_THROWS(mat(z,x,symbA,symbB));
-    CHECK_THROWS(mat(a,x,b,symbB));
-    CHECK_THROWS(mat(z,x,symbA,z));
-    CHECK_THROWS(mat(z,x,b,symbB));
-    CHECK_THROWS(mat(z,symbA,symbA,symbB));
-    CHECK_THROWS(mat(z,z,symbA,z)); 
-    CHECK_NOTHROW(mat(b,b,symbA,symbA)); // should work? haven't received clear instruction about same symbol
+    CHECK_THROWS(mat(symbA,odd_number1,odd_number1,symbB));
+    CHECK_THROWS(mat(odd_number1,symbB,odd_number1,symbB));
+    CHECK_NOTHROW(mat(odd_number1,odd_number1,symbA,odd_number1)); // should print symbA filled matrix size of b*b
+    CHECK_THROWS(mat(fraction,nega_fraction,symbA,symbB));
+    CHECK_THROWS(mat(nega_odd_number1,nega_fraction,odd_number1,symbB));
+    CHECK_THROWS(mat(fraction,nega_fraction,symbA,fraction));
+    CHECK_THROWS(mat(fraction,nega_fraction,odd_number1,symbB));
+    CHECK_THROWS(mat(fraction,symbA,symbA,symbB));
+    CHECK_THROWS(mat(fraction,fraction,symbA,fraction)); 
+    CHECK_NOTHROW(mat(odd_number1,odd_number1,symbA,symbA)); // should work? haven't received clear instruction about same symbol
 }
 
 /**
@@ -96,11 +95,10 @@ TEST_CASE("Special characters arguments"){
  * 
  */
 TEST_CASE("Zero in ROWS & COLS") {
-    int a = oddIntegerGenerator(false), b = oddIntegerGenerator(true);
-    CHECK_THROWS(mat(0,b,symbA,symbB));
-    CHECK_THROWS(mat(0,a,symbA,symbB));
-    CHECK_THROWS(mat(b,0,symbA,symbB));
-    CHECK_THROWS(mat(a,0,symbA,symbB));
+    CHECK_THROWS(mat(0,odd_number1,symbA,symbB));
+    CHECK_THROWS(mat(0,nega_odd_number1,symbA,symbB));
+    CHECK_THROWS(mat(odd_number1,0,symbA,symbB));
+    CHECK_THROWS(mat(nega_odd_number1,0,symbA,symbB));
     CHECK_THROWS(mat(0,0,symbA,symbB));
 
 }
@@ -110,14 +108,15 @@ TEST_CASE("Zero in ROWS & COLS") {
  * 
  */
 TEST_CASE("Row or Cols are 1"){
-    int a = evenIntegerGenerator(true);
-    int b = oddIntegerGenerator(true);
-    CHECK_THROWS(mat(1,a,symbA,symbB));  //size is EVEN, 1 * even number = even number
-    CHECK_THROWS(mat(a,1,symbA,symbB));  //size is EVEN, 1 * even number = even number
-    CHECK_THROWS(mat(-a,1,symbA,symbB));  // negative number & 1
-    CHECK_THROWS(mat(1,-a,symbA,symbB));  // negative number & 1
-    CHECK_NOTHROW(mat(1,b,symbA,symbB)); //size is ODD 1 * odd number = odd number
-    CHECK_NOTHROW(mat(b,1,symbA,symbB)); //size is ODD 1 * odd number = odd number   
+    CHECK_THROWS(mat(1,even_number1,symbA,symbB));  //size is EVEN, 1 * even number = even number 
+    CHECK_THROWS(mat(even_number1,1,symbA,symbB));  //size is EVEN, 1 * even number = even number
+    CHECK_THROWS(mat(nega_even_number1,1,symbA,symbB));  // negative even number & 1
+    CHECK_THROWS(mat(1,nega_even_number1,symbA,symbB));  // negative even number & 1
+    CHECK_NOTHROW(mat(1,odd_number1,symbA,symbB)); //size is ODD 1 * odd number = odd number
+    CHECK_NOTHROW(mat(odd_number2,1,symbA,symbB)); //size is ODD 1 * odd number = odd number   
+    CHECK_THROWS(mat(nega_odd_number1,1,symbA,symbB));  // negative odd number & 1
+    CHECK_THROWS(mat(1,nega_odd_number1,symbA,symbB));  // negative odd number & 1
+    
 }
 
 /**
@@ -125,37 +124,31 @@ TEST_CASE("Row or Cols are 1"){
  * 
  */
 TEST_CASE("Fractions ROWS & COLS"){
-    float a = floatGenerator(true), b = floatGenerator(false), e = floatGenerator(true), f = floatGenerator(false);
-    int c = oddIntegerGenerator(true), d = oddIntegerGenerator(false);
-    CHECK_THROWS(mat(a,c,symbA,symbB)); // positive float * positive integer
-    CHECK_THROWS(mat(c,a,symbA,symbB)); // positive integer * positive float 
-    CHECK_THROWS(mat(a,d,symbA,symbB)); // positive float * negative integer 
-    CHECK_THROWS(mat(d,a,symbA,symbB)); // negative integer * positive float 
-    CHECK_THROWS(mat(b,d,symbA,symbB)); // negative float * negative integer
-    CHECK_THROWS(mat(d,b,symbA,symbB)); // negative integer * negative float
-    CHECK_THROWS(mat(a,a,symbA,symbB)); // positive float * positive float -- same numbers
-    CHECK_THROWS(mat(b,b,symbA,symbB)); // negative float * negative float -- same numbers
-    CHECK_THROWS(mat(a,e,symbA,symbB)); // positive float * positive float -- different numbers
-    CHECK_THROWS(mat(b,f,symbA,symbB)); // negative float * negative float -- different numbers
-    CHECK_THROWS(mat(a,b,symbA,symbB)); // positive float * negative float
-    CHECK_THROWS(mat(b,a,symbA,symbB)); // negative float * positive float
-
-    
+    CHECK_THROWS(mat(fraction,odd_number1,symbA,symbB)); // positive float * positive integer
+    CHECK_THROWS(mat(odd_number1,fraction2,symbA,symbB)); // positive integer * positive float 
+    CHECK_THROWS(mat(fraction,nega_odd_number1,symbA,symbB)); // positive float * negative integer 
+    CHECK_THROWS(mat(nega_odd_number1,fraction,symbA,symbB)); // negative integer * positive float 
+    CHECK_THROWS(mat(nega_fraction,nega_odd_number2,symbA,symbB)); // negative float * negative integer
+    CHECK_THROWS(mat(nega_odd_number1,nega_fraction,symbA,symbB)); // negative integer * negative float
+    CHECK_THROWS(mat(fraction,fraction,symbA,symbB)); // positive float * positive float -- same numbers
+    CHECK_THROWS(mat(nega_fraction,nega_fraction,symbA,symbB)); // negative float * negative float -- same numbers
+    CHECK_THROWS(mat(fraction,odd_number1,symbA,symbB)); // positive float * positive float -- different numbers
+    CHECK_THROWS(mat(nega_fraction,nega_fraction2,symbA,symbB)); // negative float * negative float -- different numbers
+    CHECK_THROWS(mat(fraction,nega_fraction,symbA,symbB)); // positive float * negative float
+    CHECK_THROWS(mat(nega_fraction,fraction2,symbA,symbB)); // negative float * positive float
 
 }
 
 TEST_CASE("Good input") {
     CHECK(no_spaces(mat(9, 7, '@', '-')) == no_spaces("@@@@@@@@@@@@@\n@-----------@\n@-@@@@@@@@@-@\n@-----------@\n@@@@@@@@@@@@@"));
-
     CHECK(no_spaces(mat(13, 5, '@', '-')) == no_spaces("@@@@@@@@@\n@-------@\n@-@@@@@-@\n@-@---@-@\n@-@@@@@-@\n@-------@\n@@@@@@@@@"));
-    
     CHECK(no_spaces(mat(3, 5, '$', '+')) == no_spaces("$$$\n$+$\n$+$\n$+$\n$$$"));
-    
     CHECK(no_spaces(mat(7,7, '!', '$')) == no_spaces("!!!!!!!\n!$$$$$!\n!$!!!$!\n!$!$!$!\n!$!!!$!\n!$$$$$!\n!!!!!!!"));
-
-    CHECK(no_spaces(mat(3, 5, '$', '+')) == no_spaces("!!!\n!!!\n!!!\n!!!\n!!!"));
-
+    CHECK(no_spaces(mat(3, 5, '!', '!')) == no_spaces("!!!\n!!!\n!!!\n!!!\n!!!"));
     CHECK(no_spaces(mat(1, 1, '#', ')')) == no_spaces("#"));
+    CHECK(no_spaces(mat(5,3, '!', '$')) != no_spaces("!!!!\n!$$$$!\n!$!!!$!\n!$!$!$!\n!$!!!$!\n!$$$$$!\n!!!!!"));
+    CHECK(no_spaces(mat(51,3, '_', '!')) != no_spaces("___________________________________________________\n_!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!_\n___________________________________________________"));
+
 
     }
 
@@ -171,7 +164,7 @@ TEST_CASE("Good input") {
 float floatGenerator(bool type){
     srand(time(0));
     float num = ((float)rand()/(float)(RAND_MAX)) * 100.0;
-    return type == true ? num : -num;
+    return type == false ? -num : -num;
     }
 
 /**
@@ -191,7 +184,7 @@ int evenIntegerGenerator(bool type){
             break;
         }
     }
-    return type == true ? num : -num;
+    return type == false ? -num : num;
 }
 
 /**
@@ -211,7 +204,7 @@ int oddIntegerGenerator(bool type){
             break;
         }
     }
-    return type == true ? num : -num;
+    return type == false ? -num : num;
 }
 
 /**
